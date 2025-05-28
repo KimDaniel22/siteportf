16551c1cf84c2706d397023a57af901d7c9ef3e2
KimDaniel22 <kimdaniel2204@gmail.com>
2025-05-28 03:28:53 +0300

    еще правки

--- siteportfolio/siteportfolio/wsgi.py
+++ siteportfolio/siteportfolio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteportfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteportfolio.siteportfolio.settings')
 
 application = get_wsgi_application()
